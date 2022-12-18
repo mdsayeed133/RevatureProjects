3942f9dff893bfca0e5140e0d277f91149ed87c0
mdsayeed133 <120868662+mdsayeed133@users.noreply.github.com>
2022-12-17 18:48:09 -0800

    Add files via upload

---
+++ Project1/Project1/src/main/java/com/revature/utils/ConnectionUtil.java
@@ -0,0 +1,26 @@
+package com.revature.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class ConnectionUtil {
+
+    public static Connection getConnection() throws SQLException {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("problem occurred locating driver");
+        }
+
+        String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=project1";
+        String username = "postgres";
+        String password = "password";
+
+        return DriverManager.getConnection(url, username, password);
+
+    }
+
+}
+
