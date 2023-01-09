b237c02e1b9cfd3cd278be1243882bfecb997b53
fancyf33t <fancyf33t@protonmail.com>
2023-01-09 16:18:43 -0600

    adding navigation

--- Project2/react-bankapp/src/components/Header/Header.tsx
+++ Project2/react-bankapp/src/components/Header/Header.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../Header/Header.css'
 const Header = () => {
     return (
-        <div className="header-container container">
+        <div className="header-container container-fluid">
                 <div className="darkmode-switch">
                     <p>light/dark</p>
                 </div>
-                <div className="row d-flex justify-content-between">
+                <div className="row d-flex justify-content-around">
                     <div className="col-md-2">
                         <img src="rev-logo_281_29.png" alt="Revature logo" />
                     </div>
                     <div className="col-md-4">
                         <ul className="nav top-nav-list">
-                            <li className="nav-item"><a href="#link">Home</a></li>
+                            {/* links to "navigate" components */}
+                            <Link to="/Home" className="nav-item">Home</Link>
                             <li className="nav-item"><a href="#About">About</a></li>
-                            <li className="nav-item"><a href="#link">Account</a></li>
+                            <Link to="/Account" className="nav-item">Account</Link>
                         </ul>
                     </div>
                     <div className="col-md-2 login-status">
--- Project2/react-bankapp/src/components/Home/Home.tsx
+++ Project2/react-bankapp/src/components/Home/Home.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 import Header from '../Header/Header'
 
 import { FaAws, FaReact } from "react-icons/fa";
-import {SiTypescript, SiSpring, SiPostgresql, SiBootstrap} from "react-icons/si";
+import { SiTypescript, SiSpring, SiPostgresql, SiBootstrap } from "react-icons/si";
+import { Link } from 'react-router-dom';
 
 const Home: React.FC<any> = () => {
 
@@ -13,10 +14,22 @@ const Home: React.FC<any> = () => {
     return (
         <div>
             <Header />
+            <div className="banner-container container-fluid">
+                <h1>Big Banner Goes Here</h1>
+                <p>Something something motivational and empty</p>
+            </div>
             <div className="main-container container">
-                <div className="banner-container">
-                    <h1>Big Banner Goes Here</h1>
-                    <p>Something something motivational and empty</p>
+                <div className="row dividing-line d-flex justify-content-around">
+                    <div className="col-md-6 login-options">
+                        <h3 className="subheader">Not Logged In?</h3>
+                        <p className="text">If you want to make the most of the opportunities that Revature Banking can provide you, please click to login</p>
+                        <Link to="/" className="log-btn btn btn-secondary">Login</Link>
+                    </div>
+                    <div className="col-md-6 login-options">
+                    <h3 className="subheader">No Account?</h3>
+                        <p className="text">If you want to make the most of the opportunities that Revature Banking can provide you, please click to create an account</p>
+                        <button className="log-btn btn btn-secondary" id="accountBtn">Create Account</button>
+                    </div>
                 </div>
                 <div className="row dividing-line">
                     <div className="col-4 sub-info">
@@ -36,12 +49,12 @@ const Home: React.FC<any> = () => {
             <div className="container main-tech-stack">
                 <h3>Simple React Icons</h3>
                 <div className="icon-list">
-                    <SiBootstrap size="5em"/>
-                    <FaAws size="5em"/>
-                    <FaReact size="5em"/>
-                    <SiSpring size="5em"/>
-                    <SiTypescript size="5em"/>
-                    <SiPostgresql size="5em"/>
+                    <SiBootstrap size="5em" />
+                    <FaAws size="5em" />
+                    <FaReact size="5em" />
+                    <SiSpring size="5em" />
+                    <SiTypescript size="5em" />
+                    <SiPostgresql size="5em" />
                 </div>
             </div>
         </div>
