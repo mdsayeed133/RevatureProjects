b74908e8df68fb7b9a4f0c55d1027ec1e755d02d
mdsayeed133 <mdsayeed133@gmail.com>
2023-01-11 15:14:21 -0800

    status model is done

--- Project2/project2/src/main/java/com/revature/models/Status.java
+++ Project2/project2/src/main/java/com/revature/models/Status.java
@@ -12,5 +12,32 @@ public class Status {
     @Column(name = "status_type", nullable = false)
     private String statusType;
 
+    public Status() {
+    }
+
+    public Status(String statusType) {
+        this.statusType = statusType;
+    }
+
+    public Status(Long id, String statusType) {
+        this.id = id;
+        this.statusType = statusType;
+    }
+
     // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatusType() {
+        return statusType;
+    }
+
+    public void setStatusType(String statusType) {
+        this.statusType = statusType;
+    }
 }
