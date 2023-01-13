5f49be334255db73dda6caa142f5c88f023a6f3b
mdsayeed133 <mdsayeed133@gmail.com>
2023-01-13 14:07:52 -0800

    controller started

--- Project2/project2/src/main/java/com/revature/controllers/TransactionsController.java
+++ Project2/project2/src/main/java/com/revature/controllers/TransactionsController.java
@@ -1,5 +1,7 @@
 package com.revature.controllers;
 
+import com.revature.daos.TransactionsDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping()//TODO
 public class TransactionsController {
+    TransactionsDAO transactionsDAO;
+    @Autowired
+    public TransactionsController(TransactionsDAO transactionsDAO) {
+        this.transactionsDAO = transactionsDAO;
+    }
 }
