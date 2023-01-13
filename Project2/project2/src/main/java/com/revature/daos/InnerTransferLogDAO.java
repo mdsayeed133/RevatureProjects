f53de2d32b86ff465405d2a90b1ce226898482a3
Zackery Perski <zackeryperski@gmail.com>
2023-01-13 15:12:46 -0500

    Finished the InnerTransferLogDAO.

--- Project2/project2/src/main/java/com/revature/daos/InnerTransferLogDAO.java
+++ Project2/project2/src/main/java/com/revature/daos/InnerTransferLogDAO.java
@@ -4,6 +4,11 @@ import com.revature.models.InnerTransferLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
+@Repository //Finished code for transfer log lookup
 public interface InnerTransferLogDAO extends JpaRepository<InnerTransferLog, Integer> {
+    public Optional<List<InnerTransferLog>> findByFromAccountId(int fromAccountId);
+    public Optional<List<InnerTransferLog>> findByToAccountId(int toUserAccountId);
 }
