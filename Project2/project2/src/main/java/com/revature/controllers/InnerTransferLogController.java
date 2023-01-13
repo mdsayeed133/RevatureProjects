7c709d9dbc66d0cc5c860ab54ca77e77eb96832d
Zackery Perski <zackeryperski@gmail.com>
2023-01-13 17:08:32 -0500

    Finished wiring additional controllers

--- Project2/project2/src/main/java/com/revature/controllers/InnerTransferLogController.java
+++ Project2/project2/src/main/java/com/revature/controllers/InnerTransferLogController.java
@@ -1,5 +1,7 @@
 package com.revature.controllers;
 
+import com.revature.daos.InnerTransferLogDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping()//TODO
 public class InnerTransferLogController {
+    InnerTransferLogDAO itlDAO;
+
+    @Autowired
+    public InnerTransferLogController(InnerTransferLogDAO itlDAO) {
+        this.itlDAO = itlDAO;
+    }
 }
