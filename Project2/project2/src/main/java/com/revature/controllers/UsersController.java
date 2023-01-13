bb239a9f64b66732862d51b5a2b39b7074b78d94
mdsayeed133 <mdsayeed133@gmail.com>
2023-01-13 14:40:23 -0800

    user controller done

--- Project2/project2/src/main/java/com/revature/controllers/UsersController.java
+++ Project2/project2/src/main/java/com/revature/controllers/UsersController.java
@@ -2,17 +2,57 @@ package com.revature.controllers;
 
 import com.revature.daos.UsersDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping()//TODO
+@RequestMapping("/users")
 public class UsersController {
-    private UsersDAO uDAO;
+    private UsersDAO usersDAO;
 
     @Autowired
-    public  UsersController(UsersDAO uDAO) {this.uDAO = uDAO;}
+    public  UsersController(UsersDAO usersDAO) {this.usersDAO = usersDAO;}
+
+    @PutMapping("/{id}/firstname")
+    public ResponseEntity<String> updateFirstName(@PathVariable int id, @RequestParam String firstName) {
+        if(usersDAO.updateUserFirstName(id, firstName))
+            return ResponseEntity.ok().body("First name updated successfully");
+        else
+            return ResponseEntity.badRequest().body("Failed to update first name");
+    }
+
+    @PutMapping("/{id}/lastname")
+    public ResponseEntity<String> updateLastName(@PathVariable int id, @RequestParam String lastName) {
+        if(usersDAO.updateUserLastName(id, lastName))
+            return ResponseEntity.ok().body("Last name updated successfully");
+        else
+            return ResponseEntity.badRequest().body("Failed to update last name");
+    }
+
+    @PutMapping("/{id}/password")
+    public ResponseEntity<String> updatePassword(@PathVariable int id, @RequestParam String password) {
+        if(usersDAO.updateUserPassword(id, password))
+            return ResponseEntity.ok().body("Password updated successfully");
+        else
+            return ResponseEntity.badRequest().body("Failed to update password");
+    }
+
+    @PutMapping("/{id}/address")
+    public ResponseEntity<String> updateAddress(@PathVariable int id, @RequestParam String address) {
+        if(usersDAO.updateUserAddress(id, address))
+            return ResponseEntity.ok().body("Address updated successfully");
+        else
+            return ResponseEntity.badRequest().body("Failed to update address");
+    }
+
+    @PutMapping("/{id}/email")
+    public ResponseEntity<String> updateEmail(@PathVariable int id, @RequestParam String email) {
+        if(usersDAO.updateUserEmail(id, email))
+            return ResponseEntity.ok().body("Email updated successfully");
+        else
+            return ResponseEntity.badRequest().body("Failed to update email");
+    }
+
 
 }
