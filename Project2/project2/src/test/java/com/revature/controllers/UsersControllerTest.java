6d727ba044d98f3025cc2baadf7a235fcc1c3ca8
mdsayeed133 <mdsayeed133@gmail.com>
2023-01-14 20:40:52 -0800

    made some test for UsersController

--- Project2/project2/src/test/java/com/revature/controllers/UsersControllerTest.java
+++ Project2/project2/src/test/java/com/revature/controllers/UsersControllerTest.java
@@ -1,6 +1,7 @@
 package com.revature.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.revature.daos.UsersDAO;
@@ -12,6 +13,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Optional;
 
@@ -26,13 +30,36 @@ public class UsersControllerTest {
 
     @Test
     public void testGetUser() {
+        // Create a new User instance
         User user = new User(1, "username", "password", "firstName", "lastName", "address", "email");
+        // Configure UsersDAO.findById to return true
         when(usersDAO.findById(1)).thenReturn(Optional.of(user));
-
+        // Call the addUser method
         ResponseEntity<User> response = usersController.getUser(1);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo(user);
+        assertEquals(response.getStatusCode(),HttpStatus.OK);
+        assertEquals(response.getBody(),user);
+        assertEquals(response.getBody().getUsername(),"username");
     }
+
+    @Test
+    public void testAddUser() {
+        // Set up mock request
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        // Configure UsersDAO.addUser to return true
+        when(usersDAO.save(any(User.class))).thenReturn(new User());
+
+        // Create a new User instance
+        User user = new User(1, "John", "Doe", "johndoe@example.com", "password", "address", "email");
+
+        // Call the addUser method
+        ResponseEntity<User> response = usersController.addUser(user);
+
+        // Verify the response
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
 
 }
