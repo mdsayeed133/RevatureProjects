098c397699c9f6767184aeb3a99c5818a3bf0520
mdsayeed133 <mdsayeed133@gmail.com>
2023-01-17 11:23:12 -0800

    polished the TransactionsService

--- Project2/project2/src/main/java/com/revature/services/StatusService.java
+++ Project2/project2/src/main/java/com/revature/services/StatusService.java
@@ -4,11 +4,13 @@ import com.revature.models.Status;
 import com.revature.daos.StatusDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class StatusService {
 
     private StatusDAO statusDAO;
